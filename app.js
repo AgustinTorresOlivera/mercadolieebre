f5656e523a1fb54a4881ab6674c48cf2abdaddff
Luis Agustin Torres Olivera <aguchaso155@gmail.com>
2024-06-28 10:34:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-let port = 3000 || process.env.PORT
+let port = process.env.PORT || 3030
 
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
